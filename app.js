a915958f62537f3fb625b49c67aa44a6481870a0
Sakshi Ashok Patil <70052232+SP-17@users.noreply.github.com>
2022-11-26 19:14:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,412 @@
+var quiz = {
+    "JS": [{
+            "id": 1,
+            "question": "What does HTML stand for?",
+            "options": [{
+                "a": "Hyperlinks and Text Markup Language",
+                "b": "Hyper Text Markup Language",
+                "c": "Home Tool Markup Language"
+            }],
+            "answer": "Hyper Text Markup Language",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 2,
+            "question": "Inside which HTML element do we put the JavaScript?",
+            "options": [{
+                "a": "&lt;script&gt;",
+                "b": "&lt;javascript&gt;",
+                "c": "&lt;scripting&gt;",
+                "d": "&lt;js&gt;"
+            }],
+            "answer": "&lt;script&gt;",
+            "score": 0,
+            "status": ""
+        },
+
+        {
+            "id": 3,
+            "question": "Choose the correct HTML tag for the largest heading",
+            "options": [{
+                "a": "&lt;heading&gt;",
+                "b": "&lt;h6&gt;",
+                "c": "&lt;head&gt;",
+                "d": "&lt;h1&gt;"
+            }],
+            "answer": "&lt;h1&gt;",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 4,
+            "question": "The external JavaScript file must contain the &lt;script&gt; tag.",
+            "options": [{
+                "a": "True",
+                "b": "False"
+            }],
+            "answer": "False",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 5,
+            "question": "How do you write &quot;Hello World&quot; in an alert box?",
+            "options": [{
+                "a": "alertBox(&quot;Hello World&quot;);",
+                "b": "msg(&quot;Hello World&quot;);",
+                "c": "alert(&quot;Hello World&quot;);",
+                "d": "msgBox(&quot;Hello World&quot;);",
+            }],
+            "answer": "alert(&quot;Hello World&quot;);",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 6,
+            "question": "What is the correct HTML tag for inserting a line break?",
+            "options": [{
+                "a": "&lt;br&gt;",
+                "b": "&lt;lb&gt;",
+                "c": "&lt;bl&gt;",
+                "c": "&lt;break&gt;"
+            }],
+            "answer": "&lt;br&gt;",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 7,
+            "question": "Which of these tags are all <table> tags?",
+            "options": [{
+                "a": "&lt;table&gt; &lt;head&gt; &lt;tfoot&gt;",
+                "b": "&lt;table&gt; &lt;tr&gt; &lt;td&gt;",
+                "c": "&lt;thead&gt; &lt;body&gt; &lt;tr&gt;",
+                "d": "&lt;table&gt; &lt;tr&gt; &lt;tt&gt;"
+            }],
+            "answer": "&lt;table&gt; &lt;tr&gt; &lt;td&gt;",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 8,
+            "question": "How can you make a numbered list?",
+            "options": [{
+                "a": "&lt;ol&gt;",
+                "b": "&lt;ul&gt;",
+                "c": "&lt;dl&gt;",
+                "d": "&lt;list&gt;",
+            }],
+            "answer": "&lt;ol&gt;",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 9,
+            "question": "Which of the following is a disadvantage of using JavaScript?",
+            "options": [{
+                "a": "Client-side JavaScript does not allow the reading or writing of files.",
+                "b": "JavaScript can not be used for Networking applications because there is no such support available.",
+                "c": "JavaScript doesn't have any multithreading or multiprocess capabilities.",
+                "d": "All of the above."
+            }],
+            "answer": "All of the above.",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 10,
+            "question": "What is the correct HTML for making a checkbox?",
+            "options": [{
+                "a": "&lt;checkbox&gt;",
+                "b": "&lt;input type=&quot;checkbox&quot;&gt;",
+                "c": "&lt;check&lt;",
+                "d": "&lt;input type=&quot;check&quot;&gt;",
+            }],
+            "answer": "&lt;input type=&quot;checkbox&quot;&gt;",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 11,
+            "question": "What does CSS stand for?",
+            "options": [{
+                "a": "Creative Style Sheets",
+                "b": "Cascading Style Sheets",
+                "c": "Computer Style Sheets",
+                "d": "Colorful Style Sheets"
+            }],
+            "answer": "Cascading Style Sheets",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 12,
+            "question": "What is the correct HTML for referring to an external style sheet?",
+            "options": [{
+                "a": "&lt;stylesheet&gt; mystyle.css &lt;/stylesheet&gt;",
+                "b": "&lt;style src=&quot;mystyle.css&quot;&gt;",
+                "c": "&lt;link rel=&quot;stylesheet&quot; type=&quot;text/css&quot; href=&quot;mystyle.css&quot;&gt;"
+            }],
+            "answer": "&lt;link rel=&quot;stylesheet&quot; type=&quot;text/css&quot; href=&quot;mystyle.css&quot;&gt;",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 13,
+            "question": "How can you add a comment in a JavaScript?",
+            "options": [{
+                "a": "//This is a comment",
+                "b": "&sbquo;This is a comment",
+                "c": "&lt;!--This is a comment--&gt;"
+            }],
+            "answer": "//This is a comment",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 14,
+            "question": "How to insert a comment that has more than one line?",
+            "options": [{
+                "a": "/*This comment has more than one line*/",
+                "b": "//This comment has more than one line//",
+                "c": "&lt;!--This comment has more than one line--&gt;"
+            }],
+            "answer": "/*This comment has more than one line*/",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 15,
+            "question": "Which property is used to change the background color?",
+            "options": [{
+                "a": "color",
+                "b": "background-color",
+                "c": "bgcolor"
+            }],
+            "answer": "background-color",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 16,
+            "question": "How do you round the number 7.25, to the nearest integer?",
+            "options": [{
+                "a": "rnd(7.25)",
+                "b": "Math.round(7.25)",
+                "c": "Math.rnd(7.25)",
+                "d": "round(7.25)"
+            }],
+            "answer": "Math.round(7.25)",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 17,
+            "question": "How do you find the number with the highest value of x and y?",
+            "options": [{
+                "a": "Math.max(x, y)",
+                "b": "Math.ceil(x, y)",
+                "c": "top(x, y)",
+                "d": "ceil(x, y)"
+            }],
+            "answer": "Math.max(x, y)",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 18,
+            "question": "Which property is used to change the left margin of an element?",
+            "options": [{
+                "a": "padding-left",
+                "b": "margin-left",
+                "c": "indent"
+            }],
+            "answer": "margin-left",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 19,
+            "question": "jQuery uses CSS selectors to select elements",
+            "options": [{
+                "a": "True",
+                "b": "False"
+            }],
+            "answer": "True",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 20,
+            "question": "Which sign does jQuery use as a shortcut for jQuery?",
+            "options": [{
+                "a": "The ? sign",
+                "b": "The # sign",
+                "c": "The $ sign"
+            }],
+            "answer": "the $ sign",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 21,
+            "question": "Which event occurs when the user clicks on an HTML element?",
+            "options": [{
+                "a": "onchange",
+                "b": "onclick",
+                "c": "onmouseclick",
+                "d": "onmouseover"
+            }],
+            "answer": "onclick",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 22,
+            "question": "How do you declare a JavaScript variable?",
+            "options": [{
+                "a": "var carName;",
+                "b": "variable carName;",
+                "c": "v carName;"
+            }],
+            "answer": "var carName;",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 23,
+            "question": "Which operator is used to assign a value to a variable?",
+            "options": [{
+                "a": "*",
+                "b": "-",
+                "c": "=",
+                "d": "x"
+            }],
+            "answer": "=",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 24,
+            "question": "What will the following code return: Boolean(10 &gt; 9)",
+            "options": [{
+                "a": "NaN",
+                "b": "False",
+                "c": "True"
+            }],
+            "answer": "True",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 25,
+            "question": "Is JavaScript case-sensitive?",
+            "options": [{
+                "a": "No",
+                "b": "Yes"
+            }],
+            "answer": "Yes",
+            "score": 0,
+            "status": ""
+        }
+    ]
+}
+var quizApp = function() {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
+    this.displayQuiz = function(cque) {
+        this.currentque = cque;
+        if (this.currentque < totalque) {
+            $("#tque").html(totalque);
+            $("#previous").attr("disabled", false);
+            $("#next").attr("disabled", false);
+            $("#qid").html(quiz.JS[this.currentque].id + '.');
+            $("#question").html(quiz.JS[this.currentque].question);
+            $("#question-options").html("");
+            for (var key in quiz.JS[this.currentque].options[0]) {
+                if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+                    $("#question-options").append(
+                        "<div class='form-check option-block'>" +
+                        "<label class='form-check-label'>" +
+                        "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+                        quiz.JS[this.currentque].options[0][key] +
+                        "</span></label>"
+                    );
+                }
+            }
+        }
+        if (this.currentque <= 0) {
+            $("#previous").attr("disabled", true);
+        }
+        if (this.currentque >= totalque) {
+            $('#next').attr('disabled', true);
+            for (var i = 0; i < totalque; i++) {
+                this.score = this.score + quiz.JS[i].score;
+            }
+            return this.showResult(this.score);
+        }
+    }
+    this.showResult = function(scr) {
+        $("#result").addClass('result');
+        $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+        for (var j = 0; j < totalque; j++) {
+            var res;
+            if (quiz.JS[j].score == 0) {
+                res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+            } else {
+                res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+            }
+            $("#result").append(
+                '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+                '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+                '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+                '</div>'
+            );
+        }
+    }
+    this.checkAnswer = function(option) {
+        var answer = quiz.JS[this.currentque].answer;
+        option = option.replace(/</g, "&lt;") //for <
+        option = option.replace(/>/g, "&gt;") //for >
+        option = option.replace(/"/g, "&quot;")
+        if (option == quiz.JS[this.currentque].answer) {
+            if (quiz.JS[this.currentque].score == "") {
+                quiz.JS[this.currentque].score = 1;
+                quiz.JS[this.currentque].status = "correct";
+            }
+        } else {
+            quiz.JS[this.currentque].status = "wrong";
+        }
+    }
+    this.changeQuestion = function(cque) {
+        this.currentque = this.currentque + cque;
+        this.displayQuiz(this.currentque);
+    }
+}
+var jsq = new quizApp();
+var selectedopt;
+$(document).ready(function() {
+    jsq.displayQuiz(0);
+    $('#question-options').on('change', 'input[type=radio][name=option]', function(e) {
+        //var radio = $(this).find('input:radio');
+        $(this).prop("checked", true);
+        selectedopt = $(this).val();
+    });
+});
+$('#next').click(function(e) {
+    e.preventDefault();
+    if (selectedopt) {
+        jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(1);
+});
+$('#previous').click(function(e) {
+    e.preventDefault();
+    if (selectedopt) {
+        jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(-1);
+});
